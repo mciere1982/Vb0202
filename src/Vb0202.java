e681e472be7f0c78b99c25bafc2c05cdd3a2c7af
mciere1982 <37937144+mciere1982@users.noreply.github.com>
2020-06-29 16:28:06 +0200

    Initial commit

---
+++ src/Vb0202.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Vb0202 extends JFrame
+{
+    public static void main(String args[])
+    {
+        JFrame frame = new Vb0202();
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Voorbeeld 0202");
+        JPanel paneel = new Paneel();
+        frame.setContentPane(paneel);
+        frame.setVisible(true);
+    }
+}
+
+class Paneel extends JPanel
+{
+    private JButton knop;
+    private JTextField tekstvak;
+
+    public Paneel()
+    {
+        knop = new JButton("Klik");
+        tekstvak = new JTextField(10);
+        add(knop);
+        add(tekstvak);
+    }
+}
+
+
+
+
+
